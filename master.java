05e03b7c24fde9556c480f95374c0a8f27d2a917
Michał Lenard <lenardmichal02@gmail.com>
2024-04-06 13:44:02 +0200

    first commit

--- master.java
+++
@@ -1,5 +0,0 @@
-    class master {
-        public static void main(String[] args) {
-            System.out.println("Hello, World!");
-        }
-    }
---
+++ zadania/zad1.java
@@ -0,0 +1,10 @@
+package zadania;
+public class zad1 {
+    public static void main(String[] args) {
+        String fullName = "Michał Lenard";
+        int studentCode = 51321;
+
+        System.out.println(fullName + " " + studentCode);
+
+    }
+}
